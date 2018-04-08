606194f5da761b4dd35e8bc0960ab64784cd4d67
miaborjaac <miaborjaac@unal.edu.co>
2018-04-07 22:23:47 -0500

    Queries for the user profile (user information, my vehicles).

--- src/home.js
+++ src/home.js
@@ -4,46 +4,16 @@ import gql from "graphql-tag";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 
-
-const client = new ApolloClient({
-  uri: "http://192.168.99.101:8000/graphql"
-});
-
-
-const GET_MY_CARS = gql`
-  {
-    myVehicles(user:1){
-      plate
-      brand
-      color
-    }
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div className="body">
+          <h1>Página principal</h1>
+        </div>
+      </div>
+    );
   }
-`;
-client.query({query: GET_MY_CARS}).then(data => console.log(data));
-
-const MyVehicles = () => (
-  <Query query={GET_MY_CARS}>
-    {({ loading, error, data }) => {
-      if (loading) return "CARGANDO TUS VEHÍCULOS...";
-      if (error) return `Error! ${error.message}`;
-
-      return (
-        <select name="VEHICLE">
-          {data.myVehicles.map(vehicle => (
-            <option key={vehicle.plate} value={vehicle.brand}>
-              {vehicle.brand} - {vehicle.color}
-            </option>
-          ))}
-        </select>
-      );
-    }}
-  </Query>
-);
-
-const Home = () => (
-  <ApolloProvider client={client}>
-    <MyVehicles/>
-  </ApolloProvider>
-);
+}
 
 export default Home;
