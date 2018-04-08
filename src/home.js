dc8142bf8a7aaa3e737ecf09040bf41713bf8fc1
miaborjaac <miaborjaac@unal.edu.co>
2018-04-07 22:32:15 -0500

    Queries for the user profile (user information, my vehicles).

--- src/home.js
+++ src/home.js
@@ -9,7 +9,7 @@ class Home extends Component {
     return (
       <div>
         <div className="body">
-          <h1>Página principal</h1>
+          <h1>Página principal.</h1>
         </div>
       </div>
     );
